b4ce5229e286c2c8fb1cab8f62e91cc8ee1f42b3
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-06-02 16:57:17 -0700

    Initial version

---
+++ js/main.js
@@ -0,0 +1,18 @@
+AOS.init();
+
+
+$(document).ready(function(){
+
+  let navBar = $(".nav-bar");
+
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 100) {
+	    navBar.addClass("nav-scroll");
+	  }
+
+	  else{
+		  navBar.removeClass("nav-scroll");
+	  }
+  })
+})

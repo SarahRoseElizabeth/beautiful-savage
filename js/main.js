7e80333c82be433ae5d751e87de788146c6ae257
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-08-19 11:10:17 -0700

    Alert message added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 AOS.init();
+
+function systemMessage(event, message = ''){
+  const $messageBox = $('.system-message');
+  const $messageBoxText = $messageBox.children('.system-message-text');
+  const inputMessage = message ? message : "Want to see more? This link is just a mockup link. Scroll down to view the project.";
+  $messageBox.addClass('showing').removeClass('hidden');
+  $messageBoxText.text(inputMessage);
+  setTimeout(function(){
+    $messageBox.removeClass('showing').addClass('hidden');
+  }, 5000);
+}
 $(document).ready(function(){
 
   let navBar = $(".nav-bar");
